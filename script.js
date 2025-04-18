f82d21e86baa49174efe382c640ec21672cb5a23
Twinsland <samdamado@gmail.com>
2025-04-18 18:55:51 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,23 @@
-const musics = ['song1.mp3', 'song2.mp3', 'song3.mp3'];
-const audio = new Audio(`assets/musics/${musics[Math.floor(Math.random() * musics.length)]}`);
-audio.play();
+
+let musics = [
+    "assets/musics/song1.mp3",
+    "assets/musics/song2.mp3",
+    "assets/musics/song3.mp3"
+];
+let currentTrack = 0;
+let audio = new Audio(musics[currentTrack]);
+
+function toggleMusic() {
+    if (audio.paused) {
+        audio.play();
+    } else {
+        audio.pause();
+    }
+}
+
+function nextTrack() {
+    audio.pause();
+    currentTrack = (currentTrack + 1) % musics.length;
+    audio = new Audio(musics[currentTrack]);
+    audio.play();
+}
